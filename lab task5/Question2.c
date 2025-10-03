a40fd38a94a4092f4e2a0ef49ca54d9df8f71e99
FastSameet <k250857@nu.edu.pk>
2025-10-04 00:19:43 +0500

    Update Question2.c

--- lab task5/Question2.c
+++ lab task5/Question2.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int age;
+	char gender[10];
+	printf("enter your age:");
+	scanf("%d", &age);
+	if(age>=18){
+		printf("enter your gender(male or female):");
+		scanf("%s", &gender);
+		if(strcmp(gender,"male")==0){
+			printf("Adult Male");
+		}
+		else if(strcmp(gender,"female")==0){
+			printf("Adult Female");
+		}
+		else{
+			printf("invalid gender input.");
+		}		
+	}
+	else{
+		printf("Minor");
+	}
+	return 0;
+}
