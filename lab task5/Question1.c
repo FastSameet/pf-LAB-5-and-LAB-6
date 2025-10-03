adf7dc3a457e5f4a96a5540bee6171e05fd977c5
FastSameet <k250857@nu.edu.pk>
2025-10-04 00:17:52 +0500

    Update Question1.c

--- lab task5/Question1.c
+++ lab task5/Question1.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main(){
+	float marks;
+	printf("enter your marks:");
+	scanf("%f",&marks);
+	if(marks>=50){
+		if(marks>=85){
+			printf("passed with distinction");
+		}
+		else{
+			printf("just passed");
+		}
+	}
+	else{
+		printf("failed");
+	}
+	return 0;	
+}
